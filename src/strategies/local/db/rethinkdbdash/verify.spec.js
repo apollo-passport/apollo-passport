0cc0bb08edcea5c6d7e4e58f6eef698d40f5db10
Gadi Cohen <dragon@wastelands.net>
2016-07-24 15:37:53 +0300

    rethinkdb local verify tests - 100% coverage

--- src/strategies/local/db/rethinkdbdash/verify.spec.js
+++ src/strategies/local/db/rethinkdbdash/verify.spec.js
@@ -14,6 +14,16 @@ describe('Strategies - local - db - rethinkdbdash - verify', () => {
   const verify = _verify.bind(context);
   const db = context.db = new RethinkDBHashDriver(r, "users");
 
+  const error = new Error('test error');
+  context.error = null;
+
+  context.comparePassword = function(pass1, pass2, cb) {
+    if (context.error)
+      cb(context.error);
+    else
+      cb(null, pass1 === pass2);
+  };
+
   before(async () => {
     await freshUserTable();
     await db.users.insert([
@@ -21,15 +31,34 @@ describe('Strategies - local - db - rethinkdbdash - verify', () => {
         id: "user1",
         emails: [
           { address: "test@test.com" }
-        ]
+        ],
+        password: "test123"
       }
     ]).run();
   });
 
-  it('finds a user by email and verifies password', (done) => {
+  it('should catch rethinkdb errors and pass to callback', (done) => {
 
-    verify("test@test.com", "test123", (err, user) => {
-      user.id.should.equal("user1");
+    // Super simple API that satisfied the query in verify.js but rejects.
+    context.db = {
+      filter: () => context.db,
+      limit: () => context.db,
+      r: {
+        row: () => context.db
+      },
+      contains: () => context.db.users,
+      run() {
+        return new Promise((resolve, reject) => {
+          reject(error);
+        });
+      }
+    };
+    context.db.users = context.db;
+
+    verify("foo", "bar", (err, user) => {
+      err.should.equal(error);
+      should.equal(user, undefined);
+      context.db = db; // reset for next test
       done();
     });
 
@@ -38,7 +67,37 @@ describe('Strategies - local - db - rethinkdbdash - verify', () => {
   it('should fail on no matching email', (done) => {
 
     verify("non-existing-email", "test123", (err, user) => {
-      should.equal(user, false);
+      should.equal(user, false /*, "Invalid email" */);
+      done();
+    });
+
+  });
+
+  it('should fail on no matching password', (done) => {
+
+    verify("test@test.com", "non-matching-password", (err, user) => {
+      should.equal(user, false /*, "Invalid password" */);
+      done();
+    });
+
+  });
+
+  it('should call cb(err) on a thrown error', (done) => {
+
+    context.error = error;
+    verify("test@test.com", "test123", (err, user) => {
+      err.should.equal(error);
+      should.equal(user, undefined);
+      context.error = null; // reset for next test
+      done();
+    });
+
+  });
+
+  it('return a matching user with verified password', (done) => {
+
+    verify("test@test.com", "test123", (err, user) => {
+      user.id.should.equal("user1");
       done();
     });
 
